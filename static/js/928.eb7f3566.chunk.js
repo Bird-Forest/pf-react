51f1ba1805e0cd423bf45fd363d86eab8353fabd
Bird-Forest <127691003+Bird-Forest@users.noreply.github.com>
2024-03-05 15:43:40 +0000

    Deploying to gh-pages from @ Bird-Forest/pf-react@6529e0bb1a57e591746dec5e55b074214ad05c37 🚀

---
+++ static/js/928.eb7f3566.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[928],{3983:function(e,r,t){t.r(r),t.d(r,{default:function(){return u}});t(2791);var n=t(6616),c=t(184);function u(){return(0,c.jsx)("div",{children:(0,c.jsx)(n.uN,{children:(0,c.jsx)("h3",{children:"Welcome!"})})})}}}]);
+//# sourceMappingURL=928.eb7f3566.chunk.js.map
